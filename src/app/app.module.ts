5448dbbc9adb1631e6341458d25d94def08ead07
GeekKorky <kaori03252014@gmail.com>
2018-08-25 14:41:32 +0800

    add animations

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,8 @@ import { UsersComponent } from "./users/users.component";
 import { DetailsComponent } from "./details/details.component";
 
 import { HttpClientModule } from "@angular/common/http";
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
 
 @NgModule({
   declarations: [
@@ -18,7 +20,7 @@ import { HttpClientModule } from "@angular/common/http";
     UsersComponent,
     DetailsComponent
   ],
-  imports: [BrowserModule, AppRoutingModule, HttpClientModule],
+  imports: [BrowserModule, AppRoutingModule, HttpClientModule, BrowserAnimationsModule],
   providers: [],
   bootstrap: [AppComponent]
 })
